7d759a103e2b1cee8fb7ea9254eea2b1528ad7d7
uvsq21501832 <poulmanogo.illy@ens.uvsq.fr>
2016-10-27 23:50:34 +0200

    k

---
+++ src/main/java/IP_PROJECTS/testnew1/App.java
@@ -0,0 +1,13 @@
+package IP_PROJECTS.testnew1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
